83a9bcd6989532f00d4b8134c26a956620da203a
bijan bazaz <107120645+zoptx@users.noreply.github.com>
2022-06-14 10:42:23 -0400

    Add files via upload

---
+++ 2022-06-08_Full_Complete_TicTacToe_Bijan-main/Tic-Tac-Toe-main/Bijan_Bazaz_Code_Tic_Tac_Toe/Bijan_Bazaz_Tic-Tac-Toe/Bijan_Bazaz_Tic-Tac-Toe/StartScreen.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace Bijan_Bazaz_Tic_Tac_Toe
+{
+    partial class GameMenu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.MenuTitle = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // MenuTitle
+            // 
+            this.MenuTitle.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.MenuTitle.BackColor = System.Drawing.Color.White;
+            this.MenuTitle.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.MenuTitle.Cursor = System.Windows.Forms.Cursors.Cross;
+            this.MenuTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MenuTitle.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.MenuTitle.Location = new System.Drawing.Point(107, 35);
+            this.MenuTitle.Name = "MenuTitle";
+            this.MenuTitle.Size = new System.Drawing.Size(188, 31);
+            this.MenuTitle.TabIndex = 0;
+            this.MenuTitle.Text = "TIC-TAC-TOE ";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Transparent;
+            this.button1.Location = new System.Drawing.Point(143, 222);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Jouer";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.ExecuteJeux);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Transparent;
+            this.button2.Location = new System.Drawing.Point(143, 285);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Controle";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // GameMenu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Bijan_Bazaz_Tic_Tac_Toe.Properties.Resources.dragon;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(370, 500);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.MenuTitle);
+            this.Cursor = System.Windows.Forms.Cursors.Cross;
+            this.Name = "GameMenu";
+            this.Text = "GameMenu";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox MenuTitle;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
